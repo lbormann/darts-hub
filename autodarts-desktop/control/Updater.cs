66d9166b27f44bbbefea37e1fed2451948c9da11
lbormann <lbormann@richard-mueller.de>
2023-03-27 15:13:41 +0200

    adds

--- autodarts-desktop/control/Updater.cs
+++ autodarts-desktop/control/Updater.cs
@@ -19,7 +19,7 @@ namespace autodarts_desktop.control
         // ATTRIBUTES
 
         // Increase for new build ..
-        public static readonly string version = "v0.7.11";
+        public static readonly string version = "v0.7.12";
 
         
         public static event EventHandler<ReleaseEventArgs>? NewReleaseFound;
