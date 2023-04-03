cafdac0641155f6b6d537a18972dcf794587e0b6
lbormann <lbormann@richard-mueller.de>
2023-04-03 03:55:40 +0200

    aads

--- autodarts-desktop/control/Updater.cs
+++ autodarts-desktop/control/Updater.cs
@@ -19,7 +19,7 @@ namespace autodarts_desktop.control
         // ATTRIBUTES
 
         // Increase for new build ..
-        public static readonly string version = "v0.7.19";
+        public static readonly string version = "v0.7.20";
 
         
         public static event EventHandler<ReleaseEventArgs>? NewReleaseFound;
