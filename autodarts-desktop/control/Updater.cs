20e6485e2d17dbfc82d4edd2f04ea92e4a5e80d2
lbormann <lbormann@lucabormann.de>
2023-08-16 12:58:52 +0200

    inc caller v

--- autodarts-desktop/control/Updater.cs
+++ autodarts-desktop/control/Updater.cs
@@ -19,7 +19,7 @@ namespace autodarts_desktop.control
         // ATTRIBUTES
 
         // Increase for new build ..
-        public static readonly string version = "v0.8.32";
+        public static readonly string version = "v0.8.33";
         
 
 
