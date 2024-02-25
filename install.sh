c1d862ef9f43542ac0ed0817c9e95ff9ab1157a9
lbormann <lbormann@lucabormann.de>
2024-02-26 00:47:03 +0100

    imp

--- install.sh
+++ install.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 
-# https://raw.githubusercontent.com/autodarts-desktop/releases/main/install.sh
-
-
-#!/bin/bash
-# cd
-# wget -N -P Downloads https://github.com/lbormann/autodarts-desktop/releases/latest/download/autodarts-desktop-linux-X64.zip
-# mkdir -p autodarts-desktop
-# chmod -R 777 autodarts-desktop
-# unzip -f Downloads/autodarts-desktop-linux-X64.zip -d autodarts-desktop
-# chmod +x autodarts-desktop/autodarts-desktop
-# systemctl disable autodarts.service
-# ./autodarts-desktop/autodarts-desktop
-
 
 if [[ $1 == "--uninstall" ]]; then
-    echo "Trying to remove autodarts-desktop"
+    echo "Trying to remove Autodarts-desktop"
     
-    # exit running process
     PID=$(pgrep -f "autodarts-desktop")
     if [ -z "$PID" ]; then
-        echo "Process doesn't run."
+        echo "Autodarts-desktop doesn't run."
     else
+        echo "Close Autodarts-desktop ${PID}'."
         kill $PID
         sleep 1
         if ps -p $PID > /dev/null; then
@@ -29,7 +16,6 @@ if [[ $1 == "--uninstall" ]]; then
         fi
     fi
 
-    # remove folders
     rm ~/autodarts-desktop
     exit
 fi
@@ -40,9 +26,11 @@ if [[ "$PLATFORM" = "Linux" ]]; then
     PLATFORM="linux"
 elif [[ "$PLATFORM" = "Darwin" ]]; then
     PLATFORM="macOS"
+    sudo spctl --master-disable 
 else
     echo "Platform is not 'linux', and hence is not supported by this script." && exit 1
 fi
+
 
 ARCH=$(uname -m)
 case "${ARCH}" in
@@ -52,27 +40,15 @@ case "${ARCH}" in
     *) echo "Kernel architecture '${ARCH}' is not supported." && exit 1;;
 esac
 
-# REQ_VERSION=$1
-# REQ_VERSION="${REQ_VERSION#v}"
-# if [[ "$REQ_VERSION" = "" ]]; then
-#     VERSION=$(curl -sL https://api.github.com/repos/lbormann/autodarts-desktop/releases/latest | grep tag_name | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+')
-#     echo "Installing latest version v${VERSION}."
-# else
-#     VERSION=$(curl -sL https://api.github.com/repos/lbormann/autodarts-desktop/releases | grep tag_name | grep ${REQ_VERSION} | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+\(-\(beta\|rc\)[0-9]\+\)\?' | head -1)
-#     if [[ "$VERSION" = "" ]]; then
-#         echo "Requested version v${REQ_VERSION} not found." && exit 1
-#     fi
-#     echo "Installing requested version v${VERSION}."
-# fi
 
-# Download autodarts-desktop binary and unpack to ~/autodarts-desktop
+
 mkdir -p ~/autodarts-desktop
-# autodarts0.23.2.linux-amd64.tar.gz
-# autodarts-desktop-linux-X64.zip
 echo "Downloading and extracting 'autodarts-desktop-${PLATFORM}-${ARCH}.zip' into '~/autodarts-desktop'."  
 curl -sL https://github.com/lbormann/autodarts-desktop/releases/latest/download/autodarts-desktop-${PLATFORM}-${ARCH}.zip -o autodarts-desktop.zip && unzip -o autodarts-desktop.zip -d ~/autodarts-desktop && rm autodarts-desktop.zip
 echo "Making ~/autodarts-desktop/autodarts-desktop executable."
 chmod +x ~/autodarts-desktop/autodarts-desktop
+echo "Disable autodarts.service."
 systemctl disable autodarts.service
+echo "Starting autodarts-desktop."
 ~/autodarts-desktop/autodarts-desktop
 
