21a90127e69236930c57eb16a87755818e8fcc8e
Marcel Peschka <marcelpeschka@peschi.info>
2025-03-24 20:44:14 +0100

    Caller Update

--- darts-hub/control/ProfileManager.cs
+++ darts-hub/control/ProfileManager.cs
@@ -474,7 +474,7 @@ namespace darts_hub.control
                 MacX64 = "https://github.com/lbormann/darts-caller/releases/download/v***VERSION***/darts-caller-mac",
                 MacArm64 = "https://github.com/lbormann/darts-caller/releases/download/v***VERSION***/darts-caller-mac"
             };
-            dartsCallerDownloadUrl = dartsCallerDownloadMap.GetDownloadUrlByOs("2.17.0");
+            dartsCallerDownloadUrl = dartsCallerDownloadMap.GetDownloadUrlByOs("2.17.1");
 
 
             var dartsExternDownloadMap = new DownloadMap
