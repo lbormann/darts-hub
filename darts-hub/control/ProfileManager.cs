3c241669c39d03f19b5847a4cfe86abc7c75956e
Marcel Peschka <marcelpeschka@peschi.info>
2025-03-24 21:08:09 +0100

    bugfix

--- darts-hub/control/ProfileManager.cs
+++ darts-hub/control/ProfileManager.cs
@@ -474,7 +474,7 @@ namespace darts_hub.control
                 MacX64 = "https://github.com/lbormann/darts-caller/releases/download/v***VERSION***/darts-caller-mac",
                 MacArm64 = "https://github.com/lbormann/darts-caller/releases/download/v***VERSION***/darts-caller-mac"
             };
-            dartsCallerDownloadUrl = dartsCallerDownloadMap.GetDownloadUrlByOs("2.17.1");
+            dartsCallerDownloadUrl = dartsCallerDownloadMap.GetDownloadUrlByOs("2.17.2");
 
 
             var dartsExternDownloadMap = new DownloadMap
--- darts-hub/control/Updater.cs
+++ darts-hub/control/Updater.cs
@@ -21,7 +21,7 @@ namespace darts_hub.control
         // ATTRIBUTES
 
         // Increase for new build ..
-        public static readonly string version = "b0.12.13";
+        public static readonly string version = "b0.12.14";
         
 
         public static event EventHandler<ReleaseEventArgs>? NoNewReleaseFound;
