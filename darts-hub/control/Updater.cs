5b618fd535d07f7bf17035f44fc82659f952afed
Marcel Peschka <marcelpeschka@peschi.info>
2025-03-25 09:36:57 +0100

    Version Update

--- darts-hub/control/Updater.cs
+++ darts-hub/control/Updater.cs
@@ -21,7 +21,7 @@ namespace darts_hub.control
         // ATTRIBUTES
 
         // Increase for new build ..
-        public static readonly string version = "b0.12.15";
+        public static readonly string version = "b0.12.16";
         
 
         public static event EventHandler<ReleaseEventArgs>? NoNewReleaseFound;
