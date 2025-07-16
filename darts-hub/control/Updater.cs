88ad140da43655619ac58f5f5ce9c7fbaf13c832
Marcel Peschka <marcelpeschka@peschi.info>
2025-07-16 15:51:41 +0200

    new caller beta

--- darts-hub/control/Updater.cs
+++ darts-hub/control/Updater.cs
@@ -21,7 +21,7 @@ namespace darts_hub.control
         // ATTRIBUTES
 
         // Increase for new build ..
-        public static readonly string version = "b0.13.18";
+        public static readonly string version = "b0.13.19";
         
 
         public static event EventHandler<ReleaseEventArgs>? NoNewReleaseFound;
