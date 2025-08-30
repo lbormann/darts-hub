70c379594be9c4284f26081c2a8eac38c90278c9
Marcel Peschka <marcelpeschka@peschi.info>
2025-08-30 21:58:31 +0200

    Erweiterung der Farbpalette und Versionsaktualisierung
    ✨ Erweiterte Farbvariationen in `NewSettingsContentProvider.cs` mit neuen Webfarben wie "aliceblue" und "aqua".
    🔧 Aktualisierte Versionsnummer in `Updater.cs` von "b1.0.2" auf "b1.0.3".
    🎨 Aktualisierte Farbnamen und RGB-Werte in `WledColorDefinitions.cs`, einschließlich neuer Farben wie "black" und "blue".
    🗑️ Entfernte Graustufenvariationen und fügte neue Farbnamen wie "yellow1" bis "yellow4" hinzu.
    
    -I3uLL3t

--- darts-hub/control/Updater.cs
+++ darts-hub/control/Updater.cs
@@ -21,7 +21,7 @@ namespace darts_hub.control
         // ATTRIBUTES
 
         // Increase for new build ..
-        public static readonly string version = "b1.0.2";
+        public static readonly string version = "b1.0.3";
         
 
         public static event EventHandler<ReleaseEventArgs>? NoNewReleaseFound;
