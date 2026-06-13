a333af055a77fe91a4ae6714dc3995ebeb987f5e
Marcel Peschka <marcelpeschka@peschi.info>
2026-06-13 21:33:54 +0200

    update

--- darts-hub/control/Updater.cs
+++ darts-hub/control/Updater.cs
@@ -23,7 +23,7 @@ namespace darts_hub.control
         // ATTRIBUTES
 
         // Increase for new build ..
-        public static readonly string version = "a2.0.1.11";
+        public static readonly string version = "a2.0.1.12";
         //public static readonly string version = "b1.4.1.20";
 
 
